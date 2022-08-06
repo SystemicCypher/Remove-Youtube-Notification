04db491a1268a112dfe5e2fc06eacae3eb5df848
Austin <austinloza24@gmail.com>
2022-08-05 23:37:25 -0700

    ILLUMINATI OOOooOoOO

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('clarify-box').style.display = "none";
+document.getElementById('clarify-box').style.minHeight = "0px";
+document.getElementById('clarify-box').style.height = "0px";
